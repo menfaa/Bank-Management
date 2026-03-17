75ef88d3d2a14eaf5b1d6e9276775af8646d18d4
Menfaa <U0554@in-tech.global>
2026-03-17 16:20:17 +0100

    comments

--- kunden-service/src/main/java/com/bank/kunde/config/KafkaConfig.java
+++ kunden-service/src/main/java/com/bank/kunde/config/KafkaConfig.java
@@ -1,44 +1,47 @@
 package com.bank.kunde.config;
 
-import com.bank.common.events.InhaberDeletedEvent;
-import com.bank.common.events.PaymentCompletedEvent;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.bank.common.events.InhaberDeletedEvent; // Importiert das Event InhaberDeletedEvent
+import com.bank.common.events.PaymentCompletedEvent; // Importiert das Event PaymentCompletedEvent
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.support.DefaultKafkaHeaderMapper;
-import org.springframework.kafka.support.converter.JsonMessageConverter;
-import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
-import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
+import java.util.HashMap; // Importiert HashMap für die Alias-Map
+import java.util.Map; // Importiert Map-Interface
 
-@Configuration
+import org.springframework.context.annotation.Bean; // Importiert die Bean-Annotation für Spring
+import org.springframework.context.annotation.Configuration; // Importiert die Configuration-Annotation für Spring
+import org.springframework.kafka.support.DefaultKafkaHeaderMapper; // Importiert den DefaultKafkaHeaderMapper
+import org.springframework.kafka.support.converter.JsonMessageConverter; // Importiert den JsonMessageConverter für Kafka
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper; // Importiert den DefaultJackson2JavaTypeMapper
+import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper; // Importiert das Jackson2JavaTypeMapper-Interface
+
+@Configuration // Markiert die Klasse als Konfigurationsklasse für Spring
 public class KafkaConfig {
 
-    @Bean
-    public JsonMessageConverter jsonConverter() {
-        return new JsonMessageConverter();
+    @Bean // Markiert die Methode als Bean, die von Spring verwaltet wird
+    public JsonMessageConverter jsonConverter(DefaultJackson2JavaTypeMapper typeMapper) {
+        JsonMessageConverter converter = new JsonMessageConverter(); // Erstellt einen neuen JSON-MessageConverter
+        converter.setTypeMapper(typeMapper); // Verknüpft den TypeMapper mit dem Converter (für Alias-Mappings)
+        return converter;
     }
 
-    @Bean
+    @Bean // Erstellt eine Bean für den HeaderMapper (wird für Kafka-Header benötigt)
     public DefaultKafkaHeaderMapper headerMapper() {
         return new DefaultKafkaHeaderMapper();
     }
 
     // Diese Map löst dein Problem mit den falschen Klassen-Konvertierungen
-    @Bean
+    @Bean // Erstellt eine Bean für den TypeMapper
     public DefaultJackson2JavaTypeMapper typeMapper() {
-        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
-        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
-        typeMapper.addTrustedPackages("com.bank.common.events");
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper(); // Erstellt einen neuen TypeMapper
+        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID); // Setzt die Priorität auf TYPE_ID
+        typeMapper.addTrustedPackages("com.bank.common.events"); // Erlaubt nur Events aus diesem Package
 
-        Map<String, Class<?>> mappings = new HashMap<>();
+        Map<String, Class<?>> mappings = new HashMap<>(); // Erstellt eine Map für Alias-Namen und Klassen
         // Hier definierst du die "Alias"-Namen für deine Events
-        mappings.put("inhaber_deleted", InhaberDeletedEvent.class);
-        mappings.put("payment_completed", PaymentCompletedEvent.class);
+        mappings.put("inhaber_deleted", InhaberDeletedEvent.class); // Alias für InhaberDeletedEvent
+        mappings.put("payment_completed", PaymentCompletedEvent.class); // Alias für PaymentCompletedEvent
 
-        typeMapper.setIdClassMapping(mappings);
+        typeMapper.setIdClassMapping(mappings); // Setzt die Alias-Map im TypeMapper
         return typeMapper;
     }
 }
